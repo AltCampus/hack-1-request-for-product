0625734c1b169965a70a096e62e9e2e898911f48
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2021-02-13 16:08:59 +0530

    Create basic react boiler-plate

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Header extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        return(
+            <h1>hello</h1>
+        )
+    }
+}
+
+export default Header
