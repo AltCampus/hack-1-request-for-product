46686b3070708c863ffa1f7762235fbddcebec4d
artikgupta <artig9262@gmail.com>
2021-02-14 12:33:19 +0530

    change in styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import {BrowserRouter,Route,Link,NavLink} from 'react-router-dom'
-import Header from './Header'
-import Hero from './Hero'
-import Feed from './Feed'
+import React from "react";
+import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
+import Header from "./Header";
+import Hero from "./Hero";
+import Feed from "./Feed";
 
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class Home extends React.Component{
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return(
-            <>
-             <section className='grid w-full mx-auto container grid-cols-2 font-Balsamiq'>
-               <Header />
-               <main className='  col-span-2 flex flex-col  w-full    bg-gray-100 '>
-                   <Hero />
-                   <Feed />
-               </main>
-             </section>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <section className="grid w-full mx-auto container grid-cols-2  font-Cantata">
+          <Header />
+          <main className="  col-span-2 flex flex-col  w-full    bg-gray-100 ">
+            <Hero />
+            <Feed />
+          </main>
+        </section>
+      </>
+    );
+  }
 }
 
-export default Home
+export default Home;
